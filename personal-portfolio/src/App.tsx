a4781f16fd289941dc11d35a1eed5b67e259e6f4
N4choWasTaken <thierrygmuer@outlook.com>
2022-11-12 19:26:16 +0100

    🏗️ scuffold of header is done => div for image, console and input field are correctly placed

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,7 +1,30 @@
-function App() {
+/**
+ * This code represents the header
+ */
 
+function App() {
   return (
-    <div className=""></div>
+    <>
+    <div className="grid grid-cols-3">
+      {/*Left Part*/}
+      <div className="h-screen flex flex-col items-center">
+        <div className="w-2/3 h-full bg-neutral shadow-lg"></div>
+      </div>
+
+      {/*Right Part*/}
+      <div className="h-screen col-span-2 flex flex-col items-center justify-around font-mono font-bold">
+        <div className="w-1/2 h-3/5">
+          <div className="w-full h-4/5 mockup-code shadow-lg">
+          <pre data-prefix="$"><code className="text-success">Website is ready to run!</code></pre> 
+
+          </div>
+
+          <input className="w-full h-fit rounded-lg p-1 mt-4 bg-neutral shadow-lg" placeholder="Type !help to start"/>
+
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
