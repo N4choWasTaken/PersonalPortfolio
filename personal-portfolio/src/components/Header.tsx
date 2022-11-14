b52f605af7b74ca1477210ebf56ca0e71d8ee174
N4choWasTaken <thierrygmuer@outlook.com>
2022-11-14 21:17:42 +0100

    Personal information field is added in image space

--- personal-portfolio/src/components/Header.tsx
+++ personal-portfolio/src/components/Header.tsx
@@ -7,10 +7,17 @@ import Terminal from "./Terminal";
 function Header() {
     return (
         <>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3  font-sans">
             {/*Left Part*/}
         	<div className="h-screen flex flex-col items-center">
-                <div className="w-2/3 h-full bg-neutral shadow-lg"></div>
+                <div className="w-2/3 h-full bg-neutral shadow-lg">
+					<div className="relative top-3/4 w-full h-1/6 bg-transparent shadow-lg flex flex-col justify-evenly items-center"> {/*Personal information */}
+						<h1 className="font-bold text-4xl text-primary">Thierry Pfister</h1>
+						<div className="w-full h-1/2 flex flex-row">
+
+						</div>
+					</div>
+				</div>
             </div>
 
             {/*Right Part*/}
