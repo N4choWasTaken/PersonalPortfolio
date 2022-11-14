5f824469e54720ead6444b1c17daa9f31872ea0e
N4choWasTaken <thierrygmuer@outlook.com>
2022-11-14 20:02:39 +0100

    Terminal is in an own component + onChance navigates the page to /projects

--- personal-portfolio/src/components/Header.tsx
+++ personal-portfolio/src/components/Header.tsx
@@ -2,30 +2,24 @@
  * This code represents the header
  */
 
- function Header() {
+import Terminal from "./Terminal";
+
+function Header() {
     return (
-      <>
-      <div className="grid grid-cols-3">
-        {/*Left Part*/}
-        <div className="h-screen flex flex-col items-center">
-          <div className="w-2/3 h-full bg-neutral shadow-lg"></div>
-        </div>
-  
-        {/*Right Part*/}
-        <div className="h-screen col-span-2 flex flex-col items-center justify-around font-mono font-bold">
-          <div className="w-1/2 h-3/5">
-            <div className="w-full h-4/5 mockup-code shadow-lg">
-            <pre data-prefix="$"><code className="text-success">Website is ready to run!</code></pre> 
-  
+        <>
+        <div className="grid grid-cols-3">
+            {/*Left Part*/}
+            <div className="h-screen flex flex-col items-center">
+                <div className="w-2/3 h-full bg-neutral shadow-lg"></div>
             </div>
-  
-            <input className="w-full h-fit rounded-lg p-1 mt-4 bg-neutral shadow-lg" placeholder="Type !help to start"/>
-  
-          </div>
+
+            {/*Right Part*/}
+            <div className="h-screen col-span-2 flex flex-col items-center justify-around font-mono font-bold">
+                <Terminal />
+            </div>
         </div>
-      </div>
-      </>
+        </>
     )
-  }
-  
-  export default Header
+}
+
+export default Header;
---
+++ personal-portfolio/src/components/Terminal.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+
+function Terminal() {
+    const navigation = useNavigate();
+
+    const handleTerminalInput = (command:string) => {
+        command.toLowerCase();
+    
+        switch (command) {
+            case 'cd ./projects':
+                navigation('/projects')
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+
+    return (
+        <>
+        <div className="w-1/2 h-3/5">
+            <div className="w-full h-4/5 mockup-code shadow-lg">
+                <pre data-prefix="$"><code className="text-success">Website is ready to run!</code></pre> 
+            </div>
+
+            <input className="w-full h-fit rounded-lg p-1 mt-4 bg-neutral shadow-lg" placeholder="Type !help to start" onChange={() => handleTerminalInput('cd ./projects')}/>
+
+        </div>
+        </>
+    )
+}
+
+export default Terminal;
