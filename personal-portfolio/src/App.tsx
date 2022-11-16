f946b1d12c5b704ad1ee885db6d1f98f9d8e6426
Thierry Pfister <tpfister@kpmg.com>
2022-11-16 15:30:30 +0100

    ➕ new route /about

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/About';
 import Header from './components/Header';
 import Projects from './components/Projects';
 
@@ -18,6 +19,14 @@ function App() {
           <>
             <Header />
             <Projects />
+          </>
+        )}>
+      </Route>
+
+      <Route path='/about' element={(
+          <>
+            <Header />
+            <About />
           </>
         )}>
       </Route>
---
+++ personal-portfolio/src/components/About.tsx
@@ -0,0 +1,7 @@
+function About() {
+	return (
+		<div className="h-screem w-screen bg-blue-200"></div>
+	 );
+}
+
+export default About;
